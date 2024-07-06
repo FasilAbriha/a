8b59dcad4c85ad46ccf108b37d7dbbc8044759f7
FasilAbriha <148065663+FasilAbriha@users.noreply.github.com>
2024-07-06 11:48:48 +0300

    npm install

--- server/index.js
+++ server/index.js
@@ -1,19 +1,11 @@
 const express=require('express')
-const mongoose = require('mongoose');
-
-
-
-
-
-const app=express()const express=require('express')
 const db_connect = require('./config/db_config');
-
 const errorHandler = require('./middleware/errorHandler');
 const dotenv=require("dotenv").config()
 const tokenHandler = require("./middleware/tokenHandler");
 const mongoose = require('mongoose');
 
-//const route= express.Router()
+
 const app=express()
 
 app.use(express.json());
@@ -21,8 +13,9 @@ app.use(express.json());
 
 
 app.use(errorHandler);
-
+app.use(dotenv);
 app.use(tokenHandler);
+app.use(db_connect);
 
 const port=process.env.PORT_NUMBER;
 
