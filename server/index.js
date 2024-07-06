38224981cf3f05bb6df4a931a67643eb1cf6a0a4
FasilAbriha <148065663+FasilAbriha@users.noreply.github.com>
2024-07-06 12:10:52 +0300

    connect to database

--- server/index.js
+++ server/index.js
@@ -1,25 +1,23 @@
-const express=require('express')
+const express = require('express');
 const db_connect = require('./config/db_config');
 const errorHandler = require('./middleware/errorHandler');
-const dotenv=require("dotenv").config()
+const dotenv = require("dotenv").config();  
 const tokenHandler = require("./middleware/tokenHandler");
 const mongoose = require('mongoose');
 
-
-const app=express()
+const app = express();
 
 app.use(express.json());
 
 
-
 app.use(errorHandler);
-app.use(dotenv);
 app.use(tokenHandler);
-app.use(db_connect);
-
-const port=process.env.PORT_NUMBER;
 
 
-app.listen (port,()=> {
-    console.log(`Server listing or running on port ${port}`);    
-})
+db_connect();
+
+const port = process.env.PORT_NUMBER;
+
+app.listen(port, () => {
+    console.log(`Server listening or running on port ${port}`);
+});
